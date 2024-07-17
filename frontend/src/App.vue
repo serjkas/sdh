32b2f2039e25dd89bb0a1a7d4429ddb5334ecc47
Sergey Ka <63855768+serjkas@users.noreply.github.com>
2024-07-17 17:50:00 +0300

    example front

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router"
+import AsideComponent from "@/components/AsideComponent.vue"
+</script>
+
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <AsideComponent />
+    </el-aside>
+    <el-main>
+      <RouterView />
+    </el-main>
+  </el-container>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/AsideComponent.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { Location, Setting } from "@element-plus/icons-vue"
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<template>
+  <el-menu router :default-active="$route.path" @open="handleOpen" @close="handleClose">
+    <el-menu-item
+      :index="
+        $router.resolve({
+          name: 'home'
+        }).path
+      "
+    >
+      <el-icon>
+        <location />
+      </el-icon>
+      <span>Главная</span>
+    </el-menu-item>
+    <el-menu-item
+      :index="
+        $router.resolve({
+          name: 'test'
+        }).path
+      "
+    >
+      <el-icon>
+        <setting />
+      </el-icon>
+      <span>Тест</span>
+    </el-menu-item>
+  </el-menu>
+</template>
